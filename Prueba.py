90ff36d00eb3516864ee8a43afcbb5ff94794104
andres7925 <andresfelipe.gomez@utp.edu.co>
2025-09-09 17:08:42 -0500

    funcion factorial

--- Prueba.py
+++ Prueba.py
@@ -1,4 +1,10 @@
 def suma(numero1, numero2):
     return numero1 + numero2
 
-print(suma(5, 3))
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(500))
